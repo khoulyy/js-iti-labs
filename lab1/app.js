70c832cb923a8dc06bfaad0a5e73643529ce923e
Abdelrahman Elkhouly <khoulyyy@gmail.com>
2025-04-28 09:50:31 +0300

    q4 solution

--- lab1/app.js
+++ lab1/app.js
@@ -1,12 +1,20 @@
-console.log(number1);
-var number1 = 3;
-let number2 = 2.9;
-let number3 = 0xff;
+// console.log(number1);
+// var number1 = 3;
+// let number2 = 2.9;
+// let number3 = 0xff;
+// 
+// var firstName = "Abdelrahman";
+// var middleName = 'Abdelfattah';
+// var lastName = `Elkhouly`;
+// 
+// let flag = true;
+// 
+// console.log("This is the external javascript file");
 
-var firstName = "Abdelrahman";
-var middleName = 'Abdelfattah';
-var lastName = `Elkhouly`;
+sum(1, 2);
 
-let flag = true;
-
-console.log("This is the external javascript file");
+function sum(n1 = 0, n2 = 0) {
+    // If I inserted < 2 inputs, it will not work
+    if (typeof n1 == "number" || typeof n2 == "number") console.log("your inputs are numbers");
+    return n1 + n2;
+}
